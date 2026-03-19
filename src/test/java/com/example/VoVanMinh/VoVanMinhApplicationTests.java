96b4febff9511ef0cab217cf9b2b9b7b719f1cc8
MinhG0810 <mvo525137@gmail.com>
2026-03-19 10:55:56 +0700

    first commit

---
+++ src/test/java/com/example/VoVanMinh/VoVanMinhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VoVanMinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoVanMinhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
